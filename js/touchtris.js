a5de3b6d25774edb2b73aa2bda842f1170c16577
peter <peterned@xs4all.nl>
2010-10-09 14:46:53 +0200

    basic setup stuff

--- js/touchtris.js
+++ js/touchtris.js
@@ -1,21 +1,54 @@
+/**
+ * controller
+ * 
+ */
+
 TouchTris = new Class({
 	
-	init:function() {
+	initialize: function() {
 		// init board
 		// init emitter
 		// init pieces bin
 		// init powerups
 
+		var player1 = new Game({});
+		var player2 = new Game({});
 
+		this.addGame(player1);
+		this.addGame(player2);
+
+	},
+
+	addGame: function(game) {
+	},
+
+	start: function() {
+	},
+
+	pause: function() {
+	},
+
+	tick: function() {
 	}
-
-
-
 
 });
 
 
+/**
+ * game
+ * 
+ */
+
+Game = new Class({
+	initialize: function(options) {
+
+	}
+})
+
+
+
 $(window).addEvent('domready', function() {
 
-	$$('h1').get('slide')[0].slideOut();//setStyle('display','none');
+	window.touchtris = new TouchTris();
+
 });
