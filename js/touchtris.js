b255101dfaf79e0fbbd95281adb21c5e1b37bdda
SchizoDuckie <schizoduckie@gmail.com>
2010-10-09 14:09:43 +0200

    Setting up the main index.html

---
+++ js/touchtris.js
@@ -0,0 +1,21 @@
+TouchTris = new Class({
+	
+	init:function() {
+		// init board
+		// init emitter
+		// init pieces bin
+		// init powerups
+
+
+	}
+
+
+
+
+});
+
+
+$(window).addEvent('domready', function() {
+
+	$$('h1').get('slide')[0].slideOut();//setStyle('display','none');
+});
