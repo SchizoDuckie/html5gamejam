71736af2dc628a4bc3c155f7fec28c605fe1ecad
SchizoDuckie <schizoduckie@gmail.com>
2010-10-17 07:21:33 +0200

    Added:
    * Quick-n-Dirty user interface handling
    * Pubsub functionality to send a remote procedure call and start listening for an answer
    * Login, just once per username
    * Username is urlencoded into auth channel. This both prevents crazy usernames and allows us to target a specific client to talk to
    * Simple notifications by showMessage
    * Chat
    * Online Userlist query
    * MockTris Rendering of all clients!!! \o/
    * Rudimentary cleanup of leaving users.
    
    Signed-off-by: SchizoDuckie <schizoduckie@gmail.com>

--- 2.0/node.js/webserverroot/gameclient.js
+++ 2.0/node.js/webserverroot/gameclient.js
@@ -7,20 +7,39 @@ var GameClient = new Class({
 		this.setOptions(options);
 		$('game').get('slide').hide();
 		$('onlinewrap').get('slide').hide();
-
+		$('gamestatus').get('slide').hide();
+		$('username').focus();
 		$(this.options.loginform).addEvent('submit', this.login.bind(this));
 		$('postMessage').addEvent('submit', this.postToChat.bind(this));
- 		window.addEvent('beforeunload', this.logout.bind(this));
-//this.startGame();
+		oldUnload = window.onbeforeunload;
+		window.unbeforeunload = function() { this.logout(); oldUnload(); }.bind(this);
+	},
+
+	// execute publish an action to the server and init subCallback on subChannel on datareceive.
+	pubSub: function(pubChannel, data, subChannel, subCallback, justOnce) {
+		justOnce = justOnce || false;
+		if(justOnce) {
+
+			var subCall = subCallback;
+			var subChan = subChannel;
+			subCallback = function(message, callback) { 
+				subCall(message,callback); 
+				window.commServer.unsubscribe(subChan); 
+
+			};
+		}
+		window.commServer.subscribe(subChannel, subCallback); 			
+		window.commServer.publish(pubChannel, data);
+
 	},
 	
 	login: function(e) {
 		e.stop();
 		this.username =  $('username').get('value');
 		try
-		{
-			window.commServer.publish('/login/canihazlogin', { username:this.username, browser: [Browser.name, Browser.version] });
-			window.commServer.subscribe('/login/ihazlogdin/'+encodeURIComponent(this.username)+'/*', this.handleAuthAction.bind(this), this); 			
+		{	
+			var subChannel = '/login/ihazlogdin/'+encodeURIComponent(this.username)+'/*';
+			this.pubSub('/login/canihazlogin',  {username:this.username, browser: [Browser.name, Browser.version] }, subChannel, this.handleAuthAction.bind(this));
 		}
 		catch (E)
 		{
@@ -28,7 +47,7 @@ var GameClient = new Class({
 				if(this.username == '') {
 					this.showMessage("Please choose a username");
 				} else {
-					this.showMessage("Please choose a username without weird characters.");
+					this.showMessage("Please choose a username without any funky characters or spaces. Allowed: A-Za-z0-9-_.");
 				}
 			} else {
 				alert(E);
@@ -38,41 +57,96 @@ var GameClient = new Class({
 	
 	},
 
-	showMessage: function(msg) {
+	showLoginMessage: function(msg) {
 		if(msg) {
-			$('loginstatus').set('text', msg).get('slide').slideIn();
+			$('loginstatus').set('text', msg);
+		}
+	},
+
+	showMessage: function(msg) {
+		if(!slider) {
+			var status = $('gamestatus');
+			var slider = status.set('slide',{ link: 'chain' }).get('slide');
+			slider.hide();
+
+		}
+		if(msg) {
+			slider.slideIn();
+			status.set('html', msg);
+			slider.wait(1500).slideOut();
 		}
 	},
 
 	handleAuthAction: function(message) {
-		console.debug('New message received! ', message)
 
+		if(message.message) this.showMessage(message.message);
 		if(message.options && message.options.callBack) {
 		
 			eval("cbFunc = "+message.options.callBack+";");
 			cbFunc.bind(this)();
-
 		}
 
+	},
+
+	handleChat: function(message) {
+
+		new Element('li').set('html', '<b>'+message.username+'</b>'+new Date().getTime()+'<br>'+message.message).inject($('stream'), 'top');
 	},
 
 	logout: function() {
 		window.commServer.publish('/login/kthxbye', { username: this.username });
 	},
 
-	
 
-	userlist: function() {
-		
+	handleOnlineUsers: function(message) {
+	   if(message.message) {
+			this.showMessage(message.message)
+	   }	
+ 	   $('online').set('html', '');
+		Hash.each(message.users, function(user, data) {
+			 new Element('li').set('html', user.username+'<small>('+user.browser[0]+' v'+user.browser[1]+")</small>").injectInside($('online'));
+		});
 
+		if(message.kicked) {
+			Hash.each(message.kicked, function(user) {
+				if($(user)) { 
+					$(user).get('slide').slideIn();
+				}				
+			});
+		}
 	},
 
+	handleMockGames: function(games) {
+		if(window.console) console.debug('handling mock games!', games);
+		for (var username in games)
+		{
+			if(games[username].username && username != this.username) {
+				if(!this.mockGames[username]) {
+					var newContainer = new Element('DIV', {id: username}).injectInside($("mocks"));
+					newContainer.set('html', '<strong>'+username+'</strong>');
+					var remote = new Tetris.Remote();
 
+					this.mockGames[username] = new Tetris({
+						target: newContainer,
+						renderer:	Tetris.CanvasRenderer,
+						controller: remote,
+						cols: 11,
+						rows: 15,
+						width: 110,
+						height: 150
+					});
+				}
+				// pass the new data to the game. It registered the newData event.
+				this.mockGames[username].fireEvent('newData', games[username]);
+			}
+		}
+	},
+	
 
 	postToChat: function(e) {
 		e.stop();
-		window.commServer.publish('/game/chat/', {message: $('postMessage').value});
-		$('postMessage').value = '';
+		window.commServer.publish('/game/chat', {username: this.username, message: $('message').get('value')});
+		$('message').set('value', '');
 	},
 
 	startGame: function() {
@@ -98,7 +172,9 @@ var GameClient = new Class({
 
 
 		var renderer = Browser.name == 'ie' ? Tetris.TextRenderer : Tetris.CanvasRenderer;
-
+		if(Browser.name == 'ie') {
+			new Element('p').addClass('ieFails').set('html', 'You have an inferior browser. Now bow to our ASCII Renderer!').injectInside(document.body).get('slide').hide().slideOut();
+		}
 		var mouse = new Tetris.Mouse();
 		var player1 = new Tetris({
 			target: $('game'),
@@ -110,8 +186,23 @@ var GameClient = new Class({
 			height: 300
 		});
 
+		player1.addEvent('drop', this.publishGameState.bind(this));
+		this.mockGames = {};
+
+		this.pubSub('/game/chat', { username:this.username, message: 'Just entered the game room' }, '/game/chat', this.handleChat);
+		this.pubSub('/game/getuserlist', { username: this.username }, '/game/userlist', this.handleOnlineUsers.bind(this));
+		this.pubSub('/game/savestate/'+this.username, {username: this.username, timestamp: new Date().getTime()}, '/game/states', this.handleMockGames.bind(this));
+
+	},
+
+	cleanupKicked: function(kicked) {
+		if(window.console) console.log("Received kicked message", kicked);
+	},
+
+	publishGameState: function(state) {
+		
+		window.commServer.publish('/game/savestate/'+this.username, { username: this.username, shape: state.shape.join('|'), model: state.model.join(''), timestamp: new Date().getTime()});
 	}
-	
 
 
 });
